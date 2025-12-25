07bd8e289fde0b7e070719741c6b699b28294e2a
Nguyen Van Hung <nguyenhungyp19012004@gmail.com>
2025-12-25 08:57:50 +0700

    Sửa phù hợp codesand

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
 
-import React, {useState} from "react";
-import LoginRegister from './components/LoginRegister';
+import React, { useState } from "react";
+import LoginRegister from "./components/LoginRegister";
 import { Grid, Typography, Paper } from "@mui/material";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 import TopBar from "./components/TopBar";
 import UserDetail from "./components/UserDetail";
@@ -13,34 +18,39 @@ import UserPhotos from "./components/UserPhotos";
 const App = (props) => {
   const [loggedInUser, setLoggedInUser] = useState(null);
   return (
-      <Router>
-        <div>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TopBar user={loggedInUser} setUser={setLoggedInUser} />
-            </Grid>
-            <div className="main-topbar-buffer" />
-            <Grid item sm={3}>
-              <Paper className="main-grid-item">
-                <UserList user={loggedInUser} />
-              </Paper>
-            </Grid>
-            <Grid item sm={9}>
-              <Paper className="main-grid-item">
-                {!loggedInUser ? (
-                  <LoginRegister setUser={setLoggedInUser} />
-                ) : (
-                  <Routes>
-                    <Route path="/users/:userId" element={<UserDetail />} />
-                    <Route path="/photos/:userId" element={<UserPhotos />} />
-                  </Routes>
-                )}
-              </Paper>
-            </Grid>
+    <Router>
+      <div>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TopBar user={loggedInUser} setUser={setLoggedInUser} />
           </Grid>
-        </div>
-      </Router>
+          <div className="main-topbar-buffer" />
+          <Grid item sm={3}>
+            <Paper className="main-grid-item">
+              <UserList user={loggedInUser} />
+            </Paper>
+          </Grid>
+          <Grid item sm={9}>
+            <Paper className="main-grid-item">
+              {!loggedInUser ? (
+                <LoginRegister setUser={setLoggedInUser} />
+              ) : (
+                <Routes>
+                  <Route path="/" element={<Navigate to="/users" />} />
+                  <Route
+                    path="/users"
+                    element={<Typography>Select a user</Typography>}
+                  />
+                  <Route path="/users/:userId" element={<UserDetail />} />
+                  <Route path="/photos/:userId" element={<UserPhotos />} />
+                </Routes>
+              )}
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
